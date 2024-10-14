ab9c5a6dc8a48f96e406d4fb8b310fd07811b5f1
Dxrkd3v <dxrkd3v@ro.ru>
2024-10-14 14:45:12 +0300

    Добавил JavaScript для меню

--- pf/lp3/assets/js/main.js
+++ pf/lp3/assets/js/main.js
@@ -53,3 +53,9 @@ let swiperShoes = new Swiper('.home__swiper', {
   
 
 /*=============== SHADOW HEADER ===============*/
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
